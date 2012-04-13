af425cb6933ea91127ee52bde155f6964b3170a2
SteveXian <steve@justin.tv>
2012-04-12 17:18:49 -0700

    added alphabetical

--- pingpong/views.py
+++ pingpong/views.py
@@ -7,6 +7,7 @@ def index(request):
     if request.POST:
         elo.update(request.POST)
 
-    player_list = Player.objects.order_by("ELO").reverse();
-    return render_to_response('pingpong/index.html', {'player_list': player_list},
+    player_list = Player.objects.order_by("ELO").reverse()
+    player_list_alpha = Player.objects.order_by("player_name")
+    return render_to_response('pingpong/index.html', {'player_list': player_list, 'player_list_alpha':player_list_alpha},
         context_instance=RequestContext(request))
